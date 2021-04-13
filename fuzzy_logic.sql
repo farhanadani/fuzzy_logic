cb4c0b9b2992e81f2054e86cd2dc027d27cefaf0
farhanadani <49552441+farhanadani@users.noreply.github.com>
2021-04-13 22:42:59 +0700

    Add files via upload

--- fuzzy_logic.sql
+++ fuzzy_logic.sql
@@ -1,94 +1,119 @@
--- phpMyAdmin SQL Dump
--- version 5.0.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Waktu pembuatan: 13 Apr 2021 pada 16.13
--- Versi server: 10.4.13-MariaDB
--- Versi PHP: 7.4.8
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `fuzzy_logic`
---
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tbl_rule`
---
-
-CREATE TABLE `tbl_rule` (
-  `id_rule` varchar(11) NOT NULL,
-  `param1_name` varchar(20) NOT NULL,
-  `param1_value` varchar(20) NOT NULL,
-  `param2_name` varchar(20) NOT NULL,
-  `param2_value` varchar(20) NOT NULL,
-  `param3_name` varchar(20) NOT NULL,
-  `output` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `tbl_rule`
---
-
-INSERT INTO `tbl_rule` (`id_rule`, `param1_name`, `param1_value`, `param2_name`, `param2_value`, `param3_name`, `output`) VALUES
-('R1', 'Permintaan', 'TURUN', 'Persediaan Barang', 'BANYAK', 'Produksi', 'BERKURANG'),
-('R2', 'Permintaan', 'TURUN', 'Persediaan Barang', 'SEDIKIT', 'Produksi', 'BERKURANG'),
-('R3', 'Permintaan', 'NAIK', 'Persediaan Barang', 'BANYAK', 'Produksi', 'BERTAMBAH'),
-('R4', 'Permintaan', 'NAIK', 'Persediaan Barang', 'SEDIKIT', 'Produksi', 'BERTAMBAH');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `tbl_variabel`
---
-
-CREATE TABLE `tbl_variabel` (
-  `nama_variabel` varchar(20) NOT NULL,
-  `tipe_variabel` varchar(20) NOT NULL,
-  `bbv` varchar(40) NOT NULL,
-  `bbn` int(40) NOT NULL,
-  `bav` varchar(40) NOT NULL,
-  `ban` int(40) NOT NULL,
-  `satuan` varchar(40) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `tbl_variabel`
---
-
-INSERT INTO `tbl_variabel` (`nama_variabel`, `tipe_variabel`, `bbv`, `bbn`, `bav`, `ban`, `satuan`) VALUES
-('Permintaan', 'Input', 'Turun', 1000, 'Naik', 5000, 'Kemasan / hari'),
-('Persediaan', 'Input', 'Sedikit', 100, 'Banyak', 600, 'Kemasan / hari'),
-('Produksi', 'Output', 'Berkurang', 2000, 'Bertambah', 7000, 'Kemasan / hari');
-
---
--- Indexes for dumped tables
---
-
---
--- Indeks untuk tabel `tbl_rule`
---
-ALTER TABLE `tbl_rule`
-  ADD PRIMARY KEY (`id_rule`);
-
---
--- Indeks untuk tabel `tbl_variabel`
---
-ALTER TABLE `tbl_variabel`
-  ADD PRIMARY KEY (`nama_variabel`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+Updatecheck<|||>3<|||>1
+DoUsageStatistics<|||>3<|||>1
+ColWidths_connform.ListSessions<|||>1<|||>163,50,50,50,50,50,53
+ColsVisible_connform.ListSessions<|||>1<|||>0
+ColPositions_connform.ListSessions<|||>1<|||>0,1,2,3,4,5,6
+ColSort_connform.ListSessions<|||>1<|||>0,1
+CoolBand0Index<|||>3<|||>0
+CoolBand0Break<|||>3<|||>1
+CoolBand0Width<|||>3<|||>1360
+CoolBand1Index<|||>3<|||>1
+CoolBand1Break<|||>3<|||>1
+CoolBand1Width<|||>3<|||>1360
+MainWinOnMonitor<|||>3<|||>0
+ColWidths_MainForm.ListDatabases<|||>1<|||>150,80,50,50,50,50,50,50,50,50,120
+ColsVisible_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColPositions_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColSort_MainForm.ListDatabases<|||>1<|||>0,0
+ColWidths_MainForm.ListVariables<|||>1<|||>160,200,275
+ColsVisible_MainForm.ListVariables<|||>1<|||>0,1,2
+ColPositions_MainForm.ListVariables<|||>1<|||>0,1,2
+ColSort_MainForm.ListVariables<|||>1<|||>0,0
+ColWidths_MainForm.ListStatus<|||>1<|||>160,275,100,100
+ColsVisible_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColPositions_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColSort_MainForm.ListStatus<|||>1<|||>0,0
+ColWidths_MainForm.ListProcesses<|||>1<|||>70,80,80,80,80,50,50,145
+ColsVisible_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColPositions_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColSort_MainForm.ListProcesses<|||>1<|||>0,1
+ColWidths_MainForm.ListCommandStats<|||>1<|||>120,100,100,100,215
+ColsVisible_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColPositions_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColSort_MainForm.ListCommandStats<|||>1<|||>1,1
+ColWidths_MainForm.ListTables<|||>1<|||>120,70,70,120,120,70,100,50,70,70,70,70,70,90,120,70,70,70,50
+ColsVisible_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,18
+ColPositions_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+ColSort_MainForm.ListTables<|||>1<|||>0,0
+LastSessions<|||>1<|||>
+LastActiveSession<|||>1<|||>Unnamed
+ColWidths_frmTableEditor.listColumns<|||>1<|||>38,121,154,323,60,65,50,117,130,140,100,100
+ColsVisible_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColPositions_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColSort_frmTableEditor.listColumns<|||>1<|||>-1,0
+ColWidths_frmTableEditor.treeIndexes<|||>1<|||>434,100,80
+ColsVisible_frmTableEditor.treeIndexes<|||>1<|||>0,1,2
+ColPositions_frmTableEditor.treeIndexes<|||>1<|||>0,1,2
+ColSort_frmTableEditor.treeIndexes<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listForeignKeys<|||>1<|||>192,80,100,80,80,80
+ColsVisible_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColPositions_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColSort_frmTableEditor.listForeignKeys<|||>1<|||>-1,0
+MemoEditorWidth<|||>3<|||>387
+MainWinLeft<|||>3<|||>669
+MainWinTop<|||>3<|||>0
+UpdatecheckLastrun<|||>1<|||>2021-04-12 22:27:11
+LastUsageStatisticCall<|||>1<|||>2021-04-12 22:27:14
+FileDialogEncoding_MainForm<|||>3<|||>0
+dbtreewidth<|||>3<|||>333
+MainWinMaximized<|||>3<|||>1
+ExportSQL_CreateDatabases<|||>3<|||>1
+ExportSQL_CreateTables<|||>3<|||>1
+ExportSQL_Filenames<|||>1<|||>C:\xampp\htdocs\KTP\database\ktp.sql<{{{><}}}>C:\xampp\htdocs\TES3\tb_sku.sql<{{{><}}}>D:\Farhan\Kuliah\SEMESTER 7\KP\database\DATABASE\tb_sku.sql<{{{><}}}>D:\Farhan\Kuliah\SEMESTER 6\SISTEM PAKAR\Delphi projek\bengkelmotor.sql<{{{><}}}>D:\Farhan\Kuliah\SEMESTER 6\SISTEM PAKAR\Delphi projek\sistempakar_db.sql<{{{><}}}>
+MemoEditorHeight<|||>3<|||>292
+MainWinWidth<|||>3<|||>697
+MainWinHeight<|||>3<|||>775
+ExportSQL_DataHow<|||>3<|||>2
+DateTimeEditor_CursorPos_Type4<|||>3<|||>6
+FindDialogSearchHistory<|||>1<|||>''<{{{><}}}>farhan<{{{><}}}>
+sqloutheight<|||>3<|||>84
+DataFontSize<|||>3<|||>7
+SQLFile0<|||>1<|||>D:\Farhan\Kuliah\SEMESTER 8\PEM SIMULASI\UTS\PROJEK\fuzzy_logic.sql
+SQLFile1<|||>1<|||>D:\Farhan\Kuliah\SEMESTER 8\MANAJEMEN PROYEK\db\sampahku_db.sql
+SQLFile2<|||>1<|||>C:\xampp\htdocs\ProjekKP\tb_sku.sql
+SQLFile3<|||>1<|||>C:\xampp\htdocs\kecamatan\kecamatan.sql
+SQLFile4<|||>1<|||>D:\Farhan\Kuliah\SEMESTER 7\KP\htdocs\TES4\sku_log.sql
+SQLFile5<|||>1<|||>D:\Farhan\Kuliah\SEMESTER 7\KP\database\sku_log.sql
+SQLFile6<|||>1<|||>D:\Farhan\Kuliah\SEMESTER 7\KP\database\tb_sku.sql
+SQLFile7<|||>1<|||>D:\Farhan\Kuliah\SEMESTER 7\KP\database\datartu(3).sql
+Servers\Unnamed\SessionCreated<|||>1<|||>2019-09-17 15:53:47
+Servers\Unnamed\Host<|||>1<|||>127.0.0.1
+Servers\Unnamed\WindowsAuth<|||>3<|||>0
+Servers\Unnamed\User<|||>1<|||>root
+Servers\Unnamed\Password<|||>1<|||>7
+Servers\Unnamed\LoginPrompt<|||>3<|||>0
+Servers\Unnamed\Port<|||>1<|||>3306
+Servers\Unnamed\NetType<|||>3<|||>0
+Servers\Unnamed\Compressed<|||>3<|||>0
+Servers\Unnamed\LocalTimeZone<|||>3<|||>0
+Servers\Unnamed\QueryTimeout<|||>3<|||>0
+Servers\Unnamed\KeepAlive<|||>3<|||>0
+Servers\Unnamed\FullTableStatus<|||>3<|||>1
+Servers\Unnamed\Databases<|||>1<|||>
+Servers\Unnamed\Comment<|||>1<|||>
+Servers\Unnamed\StartupScriptFilename<|||>1<|||>
+Servers\Unnamed\TreeBackground<|||>3<|||>536870911
+Servers\Unnamed\SSHtunnelHost<|||>1<|||>
+Servers\Unnamed\SSHtunnelHostPort<|||>3<|||>0
+Servers\Unnamed\SSHtunnelUser<|||>1<|||>
+Servers\Unnamed\SSHtunnelPassword<|||>1<|||>6
+Servers\Unnamed\SSHtunnelTimeout<|||>3<|||>4
+Servers\Unnamed\SSHtunnelPrivateKey<|||>1<|||>
+Servers\Unnamed\SSHtunnelPort<|||>3<|||>3307
+Servers\Unnamed\SSL_Active<|||>3<|||>0
+Servers\Unnamed\SSL_Key<|||>1<|||>
+Servers\Unnamed\SSL_Cert<|||>1<|||>
+Servers\Unnamed\SSL_CA<|||>1<|||>
+Servers\Unnamed\SSL_Cipher<|||>1<|||>
+Servers\Unnamed\RefusedCount<|||>3<|||>21
+Servers\Unnamed\ServerVersionFull<|||>1<|||>10.4.13-MariaDB - mariadb.org binary distribution
+Servers\Unnamed\ConnectCount<|||>3<|||>67
+Servers\Unnamed\ServerVersion<|||>3<|||>100413
+Servers\Unnamed\LastConnect<|||>1<|||>2021-04-13 16:05:42
+Servers\Unnamed\lastUsedDB<|||>1<|||>fuzzy_logic
+Servers\Unnamed\FavoriteObjects<|||>1<|||>rekweb||wp_comments<{{{><}}}>tb_sku||admin<{{{><}}}>
+Servers\Unnamed\ktp|pendaftar\Sort<|||>1<|||>0_nik|
+Servers\Unnamed\seminar|users\Sort<|||>1<|||>0_password|
+Servers\Unnamed\sku|tb_sku\Sort<|||>1<|||>0_lastupdate|
+Servers\Unnamed\tb_sku|sku_log\Sort<|||>1<|||>1_action|1_sku|0_no|
+Servers\Unnamed\tb_sku|tb_sku\Sort<|||>1<|||>1_lastupdate|
